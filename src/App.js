082e86686438e9031de2b84844d3242320d2d8da
olgakaramyseva <olgakaramyseva@mac.com>
2019-04-23 22:34:22 -0700

    added fucntions in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    clickedFriendIds: []
+  };
+
+  // Function declaration 
+  shuffleFriends(friends) {
+    for (let i = friends.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [friends[i], friends[j]] = [friends[j], friends[i]];
+    }
+    return friends;
+  }
+
+  // Function expression bc =
+  handleClick = id => {
+
+    
+    // if the clicked image is in the array of clicked images
+    // then reset
+    let hasBeenClicked = false
+    for (let i = 0; i < this.state.clickedFriendIds.length; i++) {
+      // if id === clickedFriendIds[i] set hasBeenClicked to true
+    }
+    if (hasBeenClicked) {
+      // reset game
+    } else {
+      // increase score
+    }
+    
+    // if the cliced image is not in the array of clicked images
+    // then increaseScore
+    
+    // add to array of clicked images
+    
+    this.setState({ friends: this.shuffleFriends(friends) });
+  };
+
+  resetGame = () => {
+    console.log('need to finish resetGame function')
+  }
+
+  increaseScore = () => {
+    console.log('need to write increaseScore function')
+  }
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <div>
+         <Navbar
+           />
+
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            handleClick={this.handleClick}
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
       </div>
     );
   }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav">
+                <li className="nav-item active">
+                Current Score: 
+                </li>
+                <li className="nav-item">
+                </li>
+                <li className="nav-item">
+                </li>
+                <li className="nav-item">
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
